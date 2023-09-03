3af415741901a9d36131de6db9ad775bb182e88f
omnarayan <omnarayan@robustest.com>
2023-09-04 05:05:29 +0530

    modified

--- run-golangci-lint.sh
+++ run-golangci-lint.sh
@@ -7,7 +7,5 @@ if ! command -v golangci-lint &> /dev/null ; then
     echo "please check https://github.com/golangci/golangci-lint" >&2
     exit 1
 fi
-echo "printign check "
-echo "$@"
-echo "printign check ended new "
+
 exec golangci-lint run --disable-all -E errcheck -E deadcode -E errcheck -E govet -E gosimple -E ineffassign  -E staticcheck -E structcheck -E unused -E varcheck -E bodyclose -E funlen  -E gocritic -E gosec -E maligned -E misspell -E prealloc  -E scopelint -E nakedret -E unconvert
